9c0dfd118f89a0e311682a4936cc6a4b05386e2c
ashraf-km <ashraf.km@robosoftin.com>
2020-12-07 09:18:47 +0530

    before script sh file fixes

--- src/beforeScript.sh
+++ src/beforeScript.sh
@@ -1,3 +1,3 @@
-curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
-chmod +x ./cc-test-reporter
-./cc-test-reporter before-build
+echo curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+echo chmod +x ./cc-test-reporter
+echo ./cc-test-reporter before-build
