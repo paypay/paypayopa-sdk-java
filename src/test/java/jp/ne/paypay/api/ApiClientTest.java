6fb81b6db4af70d890c2eabd4cfaa6a6abc74b24
javidlulu <t-jaavidahmed.looloo@paypay-corp.co.jp>
2021-04-07 16:33:53 +0530

    Perf Mode Implementation

--- src/test/java/jp/ne/paypay/api/ApiClientTest.java
+++ src/test/java/jp/ne/paypay/api/ApiClientTest.java
@@ -87,6 +87,18 @@ public class ApiClientTest {
     }
 
     @Test
+    public void parameterToStringTest(){
+        List<String> params = new ArrayList<>();
+        params.add("Id");
+        params.add("Name");
+        String parameterToString = apiClient.parameterToString(params);
+        Assert.assertEquals(parameterToString, "Id,Name");
+        Date date  = new GregorianCalendar(2020, Calendar.FEBRUARY, 5).getTime();
+        parameterToString = apiClient.parameterToString(date);
+        Assert.assertTrue(parameterToString.startsWith("2020-"));
+    }
+
+    @Test
     public void perfModeTest(){
         apiClient.setPerfMode(true);
         Assert.assertEquals(apiClient.getBasePath(), ApiConstants.PERF_BASE_PATH);
